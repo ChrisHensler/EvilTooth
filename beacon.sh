1240e82bdd896f9a85fb2068e091bacf4cc43c6b
Chris Hensler <18620213+ChrisHensler@users.noreply.github.com>
2019-03-15 17:56:37 -0700

    added command to change advertising frequency

--- beacon.sh
+++ beacon.sh
@@ -1,5 +1,10 @@
 sudo hciconfig 0 reset
+
+#set advertising frequncy
+sudo hcitool -i hci0 cmd 0x08 0x0006 01 00 01 00 03 00 00 00 00 00 00 00 00 07 00
+#set advertise
 sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 11 11 22 22 33 33 44 44 55 55 66 66 77 77 88 88 00 00 00 00 C8 00
+
 sudo btmgmt -i hci0 le on
 sudo btmgmt -i hci0 connectable on
 sudo btmgmt -i hci0 name MYNAME
