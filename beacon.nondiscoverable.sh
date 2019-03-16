2ece9d9382731e8cc647d6838513c14f02b790f2
Chris Hensler <18620213+ChrisHensler@users.noreply.github.com>
2019-03-16 04:33:00 -0700

    renamed SLAVE2 to nondiscoverable

--- beacon.nondiscoverable.sh
+++ beacon.nondiscoverable.sh
@@ -14,6 +14,6 @@ sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 00 1A FF 4C 00 02 15 11 11 22 22 3
 
 sudo btmgmt -i hci0 le on
 sudo btmgmt -i hci0 connectable on
-if [ ! -z $1 ]; then sudo btmgmt -i hci0 name SLAVE2; fi
+if [ ! -z $1 ]; then sudo btmgmt -i hci0 name $1; fi
 sudo btmgmt -i hci0 advertising on
 sudo btmgmt -i hci0 power on
