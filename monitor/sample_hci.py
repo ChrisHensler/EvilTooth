a5fa709a6f22b52f208b6e45d13bf32f2d22d53f
ChrisHensler <18620213+ChrisHensler@users.noreply.github.com>
2019-03-19 14:01:26 -0400

    added more monitoring

--- monitor/sample_hci.py
+++ monitor/sample_hci.py
@@ -1,6 +1,8 @@
 from subprocess import run
+import datetime
 
 hci_output=''
+prev_occurances = {}
 while(1) :
     #scan ble
     hci_output = run(args=['sudo', 'timeout','5','hcitool','lescan','--duplicates'])
@@ -23,9 +25,25 @@ while(1) :
 
             addr_to_occurances[addr] += 1
 
+
+        #record address to names
+        time_recorded = datetime.datetime.now()
         for addr in addr_to_names:
-            if(len(addr_to_names) > 1):
-                print(addr + " has multiple names, this may be indicative of an attack")
+            with open("scans/names" + addr + ".txt") as f_name:
+                with open("scans/occurances" + addr + ".txt") as f_occur:
+                    f_name.write("%s\t%s", time_recorded, len(addr_to_names))
+                    f_occur.write("%s\t%s", time_recorded, addr_to_occurances[addr])
+
+                    if(len(addr_to_names) > 1):
+                        print(addr + " has multiple names, this may be indicative of an attack")
+
+                    if addr_to_occurances[addr]/prev_occurances[addr] > 3:
+                        print(addr + " has has seen a major spike in activity, this may be indicative of an attack")
+
+                    prev_occurances[addr] = addr_to_occurances[addr]
+
+
+                
 
 
 
