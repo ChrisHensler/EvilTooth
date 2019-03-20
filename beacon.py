43bb42ebbb2ae4aba8b5ac41e5d4a84d34db4f0f
ChrisHensler <18620213+ChrisHensler@users.noreply.github.com>
2019-03-20 11:13:37 -0400

    fixed a lot of debug related errors

--- beacon.py
+++ beacon.py
@@ -34,31 +34,31 @@ def int_to_hexstring(i):
     return s2[0: 5]
 
 
-max_interval = int_to_hexstring(interval+1).split(' ')
-min_interval = int_to_hexstring(interval).split(' ')
+max_interval = int_to_hexstring(interval+1)
+min_interval = int_to_hexstring(interval)
 
 print("Advertising Address: %s,\n Min Interval: %s,\nMax Interval: %s\n"%(advertising_address, min_interval, max_interval))
 
 cmd_args = ['sudo', 'hcitool','-i','hci0','cmd']
 cmd_args.append('0x08')                                     #ogf
 cmd_args.append('0x0006')                                   #ocf
-cmd_args += min_interval                                    #min_interval
-cmd_args += max_interval                                    #max_interval
+cmd_args += min_interval.split(' ')                                    #min_interval
+cmd_args += max_interval.split(' ')                                    #max_interval
 cmd_args += '00'
 cmd_args += '00'
 cmd_args += '00'
 cmd_args += advertising_address.split(':')                  #address
 cmd_args += '01'
 cmd_args += '00'
-print(run(args=cmd_args))
+run(args=cmd_args)
 
 #set advertise ##TODO: dissect the command and make more configurable
-print(run(args="sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 11 11 22 22 33 33 44 44 55 55 66 66 77 77 88 88 00 00 00 00 C8 00".split(' ')))
+run(args="sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 11 11 22 22 33 33 44 44 55 55 66 66 77 77 88 88 00 00 00 00 C8 00".split(' '))
 
-print(run(args=["sudo","btmgmt", "-i", "hci0","le","on"]).stdout)
-print(run(args=["sudo","btmgmt","-i","hci0","connectable","on"]).stdout)
-print(run(args=["sudo","btmgmt","-i","hci0",name]).stdout)
-print(run(args=["sudo","btmgmt","-i","hci0","advertising","on"]).stdout)
-print(run(args=["sudo","btmgmt","-i","hci0","power","on"]).stdout)
+run(args=["sudo","btmgmt", "-i", "hci0","le","on"])
+run(args=["sudo","btmgmt","-i","hci0","connectable","on"])
+run(args=["sudo","btmgmt","-i","hci0","name",name]).stdout
+run(args=["sudo","btmgmt","-i","hci0","advertising","on"]).stdout
+run(args=["sudo","btmgmt","-i","hci0","power","on"]).stdout
 
 print(datetime.datetime.utcnow().strftime('Advertising started: %B %d %Y - %H:%M:%S'))
