13bbeac0229fd89f78bc2f67b6284df7ac1e2fb0
ChrisHensler <18620213+ChrisHensler@users.noreply.github.com>
2019-03-31 01:35:47 -0400

    tweaked the advertising logic for april. let's see if it's more effective

--- malice/april_fools.py
+++ malice/april_fools.py
@@ -2,11 +2,11 @@ from subprocess import run,check_output,PIPE, Popen,call
 import datetime
 
 #takes function with 1 param
-def celebrate(on_new_addr):
+def celebrate(adv_func):
     #scan ble
     run(args=['sudo', "hciconfig", "hci0", "down"])
     run(args=['sudo', "hciconfig", "hci0", "up"])
-    monitor_proc = Popen(['sudo','hcitool','lescan','--duplicates'], stdout=PIPE)
+    monitor_proc = Popen(['sudo','hcitool','lescan'], stdout=PIPE)
 
     addresses = {}
 
@@ -25,7 +25,8 @@ def celebrate(on_new_addr):
             segments = line.split(' ')
             addr = segments[0]
             name = segments[1]
+            
+            addresses[addr] = 1
 
-            if(not addr in addresses):
-                on_new_addr(addr)
-                addresses[addr] = 1
+            for addr in addresses:
+                adv_func(addr)
