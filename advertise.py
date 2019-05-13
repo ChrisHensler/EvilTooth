ca549274d2684bedf9b19769b5ce6c8bc722328a
ChrisHensler <18620213+ChrisHensler@users.noreply.github.com>
2019-05-13 03:51:23 -0400

    removed the last hcitool command, I think it's resetting something

--- advertise.py
+++ advertise.py
@@ -47,5 +47,5 @@ def advertise(advertising_address='', interval=30, name=None):
     cmd_args += '00'
     run(args=cmd_args)
 
-    #set advertise ##TODO: dissect the command and make more configurable
-    run(args="sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 11 11 22 22 33 33 44 44 55 55 66 66 77 77 88 88 00 00 00 00 C8 00".split(' '))
+    ##set advertise ##TODO: dissect the command and make more configurable
+    #run(args="sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 11 11 22 22 33 33 44 44 55 55 66 66 77 77 88 88 00 00 00 00 C8 00".split(' '))
